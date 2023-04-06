c0295c0e532ce018ef32ee7d6dd3c763ab284433
mason <mason@MasonsDell.dhcp.wustl.edu>
2023-04-06 14:14:52 -0500

    :))))))))))

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,80 @@
 package studio8;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Objects;
+
 public class Date {
+public int month;
+public int day;
+public int year;
+public boolean holiday;
+
+public Date(int month1, int day1, int year1, boolean holiday1) {
+	this.month=month1;
+	this.day=day1;
+	this.holiday=holiday1;
+	this.year=year1;
+	holiday1=false;
+}
+public String toString() {
+	if(this.holiday) {
+		return "holiday! " + month + "/" + day + "/" + year;
+		}	
+	else {
+		return month + "/" + day + "/" + year;
+	}
+}
+
+ 
+    @Override
+public int hashCode() {
+	return Objects.hash(day, month, year);
+}
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Date other = (Date) obj;
+	return day == other.day && month == other.month && year == other.year;
+}
 
 
-    public static void main(String[] args) {
+
+	public static void main(String[] args) {
+    		Date mason = new Date(1, 3, 2004, false);
+    		System.out.println("Mason: " +(mason.toString()));
+        	
+    		Date tori = new Date(11,7,2004,false);
+    		System.out.println("Tori: " + (tori.toString()));
+    		
+    		Date noah= new Date(2, 6, 2004, false);
+    		System.out.println("Noah: " + (noah.toString()));
+    		
+    		Date alsoNoah = new Date(2,6,2004,false);
+    		
+    		System.out.println(noah.equals(alsoNoah));
+    	
+    		LinkedList<Date> list = new LinkedList<Date>();
+    		list.add(mason);
+    		list.add(tori);
+    		list.add(noah);
+    		list.add(noah);
+    		System.out.println(list);
+    		
+    		HashSet<Date> set = new HashSet<Date>();
+    		set.add(mason);
+    		set.add(tori);
+    		set.add(noah);
+    		set.add(noah);
+    		System.out.println(set);
 
     }
 
 }
+
+
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,11 +1,77 @@
 package studio8;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Objects;
 
 public class Time {
 
+	public int hour;
+	public int minute;
+	public boolean format;
+	
+	public Time(int hour1, int minute1, boolean format1) {
+		this.hour=hour1;
+		this.minute=minute1;
+		this.format=format1;
+		format1=false;
+		
+	}
+	
+	public String toString() {
+		if(this.format) {
+			return hour%12 + ":" + minute;
+			}	
+		else {
+			return hour + ":" + minute;
+		}
+	}
+	
+	
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(hour, minute);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Time other = (Time) obj;
+		return hour == other.hour && minute == other.minute;
+	}
+
 	public static void main(String[] args) {
- 
+		Time now = new Time(1,28,false);
+		System.out.println(now.toString());
     	
+		Time now1 = new Time (14,47, true);
+		System.out.println(now1.toString());
+		
+		Time now2= new Time(16, 58, false);
+		System.out.println(now2.toString());
+		
+		System.out.println(now.equals(now1));
+		System.out.println(now.equals(now));
+		
+		LinkedList<Time> list = new LinkedList<Time>();
+		list.add(now);
+		list.add(now1);
+		list.add(now2);
+		list.add(now2);
+		System.out.println(list);
+		
+		HashSet<Time> set = new HashSet<Time>();
+		set.add(now);
+		set.add(now1);
+		set.add(now2);
+		set.add(now2);
+		System.out.println(set);
     }
 
 }
